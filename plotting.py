b971f5ad3d7b3b71ee638160b504ba9962b467e8
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-10-20 12:38:34 -0500

    Cleaning and adding hover feature
    -Added popup hover feature, added ColumnDataSource method to add data to Bokeh plot
    -Only need to execute plotting.py by fetching df form MotionDetection.py script
    -Instead of using list, manually set 'top' and 'bottom'
    -Cleaned x-axis: removed ticks and grid

--- plotting.py
+++ plotting.py
@@ -1,22 +1,28 @@
-#execute this file after MotionDetection.py to displays times object enters and exits frame in Bokeh graph
+#execute this file to detect moving objects in webcam, press q to quit
+#automatically displays times object enters and exits frame in bokeh plot with info in popup
+from MotionDetection import df
+
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
-import pandas
+from bokeh.models import HoverTool, ColumnDataSource
 
-list=[]
-i=0
-df=pandas.read_csv("MotionTimes.csv",parse_dates=["start","end"])
-lft=df["start"]
+#convert orig cols to strings so hovertool method can display in popup
+#creates new cols Start_string and End_string
+df["Start_string"]=df["start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"]=df["end"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
-output_file("Quad.html")
-f=figure(width=400,height=400,x_axis_type="datetime")
-f.title.text="Motion Graph"
-#top and bottom always 1 and 0
-while i in range(len(lft)):
-    toplist=[1]*len(lft)
-    botlist=[0]*len(lft)
-    i=i+1
+cds=ColumnDataSource(df) #conv df to cds obj
 
-f.quad(top=toplist, bottom=botlist, left=df["start"], right=df["end"])
+output_file("MotionGraph.html")
+f=figure(width=500,height=100,x_axis_type="datetime",sizing_mode='scale_width',title="Motion Graph")
+f.yaxis.minor_tick_line_color=None
+f.yaxis.ticker.desired_num_ticks=1
+
+#takes 'tooltips' param which takes list of tuples - start string then array of data
+hover=HoverTool(tooltips=[("start","@Start_string"), ("end","@End_string")])
+f.add_tools(hover) #add tool
+
+#top and bottom always 1 and 0, pass source param as cds
+q=f.quad(left="start",right="end",bottom=0,top=1,color="green", source=cds)
 
 show(f)
